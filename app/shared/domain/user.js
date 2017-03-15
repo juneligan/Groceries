d77af76d2d74458ccbc8cfb8aeba934a8e482143
juneligan <ligan.june@gmail.com>
2017-03-16 02:00:36 +0800

    Signed-off-by: juneligan <ligan.june@gmail.com>

--- app/shared/domain/user.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var User = (function () {
-    function User() {
-    }
-    return User;
-}());
-exports.User = User;
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidXNlci5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbInVzZXIudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBO0lBQUE7SUFNQSxDQUFDO0lBQUQsV0FBQztBQUFELENBQUMsQUFORCxJQU1DO0FBTlksb0JBQUkiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgY2xhc3MgVXNlciB7XG4gIHVzZXJuYW1lOiBzdHJpbmc7XG4gIGVtYWlsQWRkcmVzczogc3RyaW5nO1xuICBwYXNzd29yZDogc3RyaW5nO1xuICBmaXJzdG5hbWU6IHN0cmluZztcbiAgbGFzdG5hbWU6IHN0cmluZztcbn0iXX0=
